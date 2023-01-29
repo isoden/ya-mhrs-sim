a37c062e4a06d646fe5533d7f556acbdcffd34f6
isoden <3771988+isoden@users.noreply.github.com>
2023-01-29 09:05:28 +0900

    doc: add comment

--- libs/data/src/lib/models/armor/armor.spec.ts
+++ libs/data/src/lib/models/armor/armor.spec.ts
@@ -2,6 +2,7 @@ import { Armor } from './armor'
 import { armors } from './armors-json'
 
 describe('Armor', () => {
+  // データの整合性チェック
   it.each(armors)('should be passed schema check $name', (armor) => {
     expect(() => Armor.parse(armor)).not.toThrow()
   })
--- libs/data/src/lib/models/decoration/decoration.spec.ts
+++ libs/data/src/lib/models/decoration/decoration.spec.ts
@@ -3,6 +3,7 @@ import { decorations } from './decorations-json'
 
 describe('Decoration', () => {
   it.each(decorations)('should be passed schema check $name', (decoration) => {
+    // データの整合性チェック
     expect(() => Decoration.parse(decoration)).not.toThrow()
   })
 })
