e659cb2a6256b7fd8e7c958339b9f96da9b3ebdd
isoden <3771988+isoden@users.noreply.github.com>
2023-02-12 04:44:28 +0900

    test: add spec

--- apps/webapp/src/app/simulator/simulator-page/components/simulator-page/simulator-page.component.spec.ts
+++ apps/webapp/src/app/simulator/simulator-page/components/simulator-page/simulator-page.component.spec.ts
@@ -1,11 +1,20 @@
-import { render, screen } from '~webapp/test-utils'
+import { render, screen, within } from '~webapp/test-utils'
 
 import { SimulatorPageComponent } from './simulator-page.component'
 
 describe('SimulatorPageComponent', () => {
   it('should render', async () => {
-    await render(SimulatorPageComponent)
+    const { user } = await render(SimulatorPageComponent)
 
-    expect(screen.getByRole('heading', { name: /スキルシミュレーター/ })).toBeInTheDocument()
+    const widget = screen.getByRole('region', { name: /検索結果/ })
+
+    // assert: 事前条件
+    expect(widget).toHaveTextContent('スキルを選択してください')
+
+    // act: 攻撃レベル7を選択
+    await user.click(screen.getByLabelText('攻撃レベル7'))
+
+    const list = await within(widget).findByRole('list', { name: /発動スキル/ })
+    expect(list).toHaveTextContent('攻撃Lv7')
   })
 })
